6cf9fffe0dead3f7d5b4055f919d45cc98516be8
enzos3 <enzos3397@gmail.com>
2023-07-10 21:09:40 -0300

    v1.2

--- components/WorldCard3.jsx
+++ components/WorldCard3.jsx
@@ -8,20 +8,20 @@ const WorldCard3 = ({ onClose }) => {
       <div className='max-w-lg w-full mx-4 bg-white rounded-lg overflow-hidden shadow-lg'>
         <div className='p-6'>
           <h1 className='text-3xl text-grey-800 font-bold text-center mb-4'>
-            SEO Developer - Fullstack Developer
+            'SEO Developer - Fullstack Developer'
           </h1>
           <h2 className='text-xl text-grey-800 text-center mb-6'>
-            SOUP Agency LTDA | January 2022 - June 2023
+            'SOUP Agency LTDA | January 2022 - June 2023'
           </h2>
           <p className='text-gray-800 text-center mb-8'>
-            The Haircare E-Commerce project is an online platform that offers a
+            'The Haircare E-Commerce project is an online platform that offers a
             wide range of hair care products. Developed with modern technologies
             like React, Node.js, and MongoDB, it prioritizes user-friendliness
             and efficiency. Key features include personalized customer profiles,
             purchase history, and a powerful search engine. The platform
             supports multiple payment methods and focuses on scalability and
             security. Measures such as data encryption and two-factor
-            authentication are implemented to protect customer data.
+            authentication are implemented to protect customer data.'
           </p>
           <div className='flex flex-row justify-around'>
             <a
